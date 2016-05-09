4c27c82b37a1585c39d2129e77ce28e85f167831
Carly Rolfes <carly.rolfes@yahoo.com>
2016-05-09 16:39:54 -0400

    Header File for the game mode

---
+++ MyGameMode.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPSPAWNTEST_API AMyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	void AMyGameMode::BeginPlay();
+	
+	
+	
+};
