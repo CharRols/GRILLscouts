76c997be25b5b7ec0e4a4d707207a9b9fd685095
Carly <carly.rolfes@yahoo.com>
2016-05-12 12:45:06 -0400

    Delete code for game mode and solidBlock

---
+++ Documents/Unreal Projects/CPPSpawnTest/Source/CPPSpawnTest/CPPSpawnTest.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CPPSpawnTest.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPPSpawnTest, "CPPSpawnTest" );
